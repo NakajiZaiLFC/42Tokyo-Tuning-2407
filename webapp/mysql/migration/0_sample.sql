31b2587493fab42df45c3ab884f7c385832dc5f6
Syota Nakajima <snakajim@c2r8s7.42tokyo.jp>
2024-07-28 13:16:08 +0900

    submit

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -16,5 +16,30 @@ CREATE INDEX idx_dispatchers_id ON dispatchers (id);
 CREATE UNIQUE INDEX idx_dispatchers_user_id ON dispatchers (user_id);
 CREATE INDEX idx_dispatchers_area_id ON dispatchers (area_id);
 
+-- areas テーブルのインデックス
+CREATE INDEX idx_areas_id ON areas (id);
+
+-- orders テーブルのインデックス
+CREATE INDEX idx_orders_id ON orders (id);
+CREATE INDEX idx_orders_dispatcher_id ON orders (dispatcher_id);
+CREATE INDEX idx_orders_area_id ON orders (area_id);
+CREATE INDEX idx_orders_status ON orders (status);
+
+-- order_details テーブルのインデックス
+CREATE INDEX idx_order_details_id ON order_details (id);
+CREATE INDEX idx_order_details_order_id ON order_details (order_id);
+
+-- order_histories テーブルのインデックス
+CREATE INDEX idx_order_histories_id ON order_histories (id);
+CREATE INDEX idx_order_histories_order_id ON order_histories (order_id);
+
+-- order_details_histories テーブルのインデックス
+CREATE INDEX idx_order_details_histories_id ON order_details_histories (id);
+CREATE INDEX idx_order_details_histories_order_detail_id ON order_details_histories (order_detail_id);
+
+
+-- 以下のように、複数のカラムにインデックスを追加することもできます
+-- CREATE INDEX idx_users_id_username ON users (id, username);
+
 -- 注意: 既存のインデックスがある場合は、以下のようにIF NOT EXISTSを使用することができます
 -- CREATE INDEX IF NOT EXISTS idx_users_id ON users (id);
